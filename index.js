98643250a44733eff45e003bd73d6b6a5e726ad6
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-05-26 13:07:05 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase()
+  })
+}
+function fuzzyMatch (list, partOfName) {
+  let nameLength = partOfName.length
+  return list.filter(function (driverName) {
+      return driverName.slice(0, nameLength) === partOfName
+  })
+}
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase()
+  })
+}
